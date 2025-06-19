5e7830876e37be75e20080e0e98ae4ed53c6b615
kim <youngwoo7804@gmail.com>
2025-06-19 19:58:42 +0900

    오큰수와 스택 수열

---
+++ src/DoitJava1_1/P17298_오큰수구하기.java
@@ -0,0 +1,34 @@
+package DoitJava1_1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+// 3 5 2 7
+// 5 7 7 -1
+public class P17298_오큰수구하기 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0); //스택 초기화
+        int N = Integer.parseInt(br.readLine());
+        int A[] = new int[N];
+        int ans[] = new int[N];
+        String str[] = br.readLine().split(" ");
+        for(int i = 0;i<N;i++){
+            A[i] = Integer.parseInt(str[i]);
+        }
+        for(int i = 0;i<N;i++){
+            while(!stack.isEmpty() && A[i] > A[stack.peek()]){ //즉 3과 5를 비교
+                ans[stack.pop()] = A[i];
+            }
+            stack.push(i);
+        }
+        while(!stack.isEmpty()){
+            //반복문을 다 돌고 나왔는데 스택이 비어 있지 않다면 빌 때까지
+            ans[stack.pop()] = -1;
+        }
+
+    }
+}
